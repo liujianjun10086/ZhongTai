005e33bfdc6fa828d7eab1f78a89e03507c5a36f
liujianjun10086 <2580695719@qq.com>
2018-04-06 20:06:27 +0800

    create

---
+++ src/main/java/cn/lanya/service/OrganizeService.java
@@ -0,0 +1,11 @@
+package cn.lanya.service;
+
+import cn.lanya.po.Organize;
+
+import java.util.List;
+
+public interface OrganizeService {
+
+       //获取所有的机构 ljj
+       List<Organize> getAll();
+}
