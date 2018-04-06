dc294c1fe47d266e8c360a8dc899381405c065d8
liujianjun10086 <2580695719@qq.com>
2018-04-06 21:51:06 +0800

    create

--- src/main/java/cn/lanya/util/DateTools.java
+++ src/main/java/cn/lanya/util/DateTools.java
@@ -1,20 +1,21 @@
 package cn.lanya.util;
 
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
 
 public class DateTools {
 
     public static Date getDate(String str){
-        SimpleDateFormat sdf =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf =new SimpleDateFormat("yyyy-MM-dd");
         Date date = null;
         try {
-            date = sdf.parse(str);
+            Date newDate =sdf.parse(str);
+            date = new Date(newDate.getTime());
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
         return date;
     }
 
