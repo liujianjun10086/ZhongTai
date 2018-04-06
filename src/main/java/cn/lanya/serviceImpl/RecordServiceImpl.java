b1d7f0b31f9901747183909258cfa9704b0c9302
liujianjun10086 <2580695719@qq.com>
2018-04-06 20:32:20 +0800

    create

--- src/main/java/cn/lanya/serviceImpl/RecordServiceImpl.java
+++ src/main/java/cn/lanya/serviceImpl/RecordServiceImpl.java
@@ -6,11 +6,18 @@ import cn.lanya.service.RecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RecordServiceImpl implements RecordService {
 
     @Autowired
     private RecordMapper recordMapper;
+
+    @Override
+    public List<Record> getAll() {
+        return recordMapper.selectAll();
+    }
 
     //新增审批
     @Override
